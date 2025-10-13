e5d40baacfc47084a209956158cd7e4e1a2fb56f
张发发 <2637375274@qq.com>
2025-10-13 10:48:51 +0800

    refactor(fonts): replace DouyinSansBold font file with optimized .woff2 version

--- packages/excalidraw/fonts/douyin/index.ts
+++ packages/excalidraw/fonts/douyin/index.ts
@@ -1,6 +1,6 @@
 import { type ExcalidrawFontFaceDescriptor } from "../Fonts";
 
-import DouyinSansBold from "./DouyinSansBold.woff";
+import DouyinSansBold from "./DouyinSansBold.woff2";
 
 export const DouyinFontFaces: ExcalidrawFontFaceDescriptor[] = [
   {
